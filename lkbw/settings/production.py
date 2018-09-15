52d8dcc157eff4923f9bb5a61b4367cda3d21253
cutreth <cutreth@users.noreply.github.com>
2018-09-15 15:55:32 -0500

    Updating PRD allowed host

--- lkbw/settings/production.py
+++ lkbw/settings/production.py
@@ -5,6 +5,8 @@ DEBUG = False
 with open('/home/lkbw/lkbw/SecretKey.txt') as key:
 	SECRET_KEY = key.read().strip()
 
+ALLOWED_HOSTS = ['lkbw.pythonanywhere.com'] 
+	
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
