8feeb45fd7646b118792447568b423539ad05bd3
cutreth <cutreth@users.noreply.github.com>
2018-09-15 16:37:34 -0500

    Rolling back DB changes

--- lkbw/settings/dev.py
+++ lkbw/settings/dev.py
@@ -11,13 +11,6 @@ ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 try:
     from .local import *
 except ImportError:
--- lkbw/settings/production.py
+++ lkbw/settings/production.py
@@ -6,16 +6,6 @@ with open('/home/lkbw/lkbw/SecretKey.txt') as key:
 	SECRET_KEY = key.read().strip()
 
 ALLOWED_HOSTS = ['lkbw.pythonanywhere.com'] 
-	
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'lkbw$default',
-		'USER': 'lkbw',
-		'PASSWORD': 'lkbwdbpw',
-		'HOST': 'lkbw.mysql.pythonanywhere-services.com',
-    }
-}
 
 try:
     from .local import *
