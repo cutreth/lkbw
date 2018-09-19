3302d56b5744406863f28cc3a59dbaa5629715d5
cutreth <cutreth@users.noreply.github.com>
2018-09-19 16:51:24 -0500

    Updating host in PRD settings

--- lkbw/settings/production.py
+++ lkbw/settings/production.py
@@ -5,7 +5,7 @@ DEBUG = False
 with open('/home/lkbw/lkbw/SecretKey.txt') as key:
 	SECRET_KEY = key.read().strip()
 
-ALLOWED_HOSTS = ['lkbw.pythonanywhere.com'] 
+ALLOWED_HOSTS = ['lkbw.pythonanywhere.com', 'lilkevbigworld.com'] 
 
 try:
     from .local import *
