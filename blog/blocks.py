6477e26d59eb807033484c9b06b6aac270749fa6
cutreth <cutreth@users.noreply.github.com>
2018-11-19 10:49:53 -0600

    Migration fixes and refactoring

---
+++ blog/blocks.py
@@ -0,0 +1,277 @@
+import json
+import six
+
+from django import forms
+from django.conf import settings
+from django.utils.functional import cached_property
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+
+try:
+    from django.contrib.gis.geos.point import Point
+except:
+    Point = None
+
+from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
+
+from wagtailgeowidget.blocks import GeoBlock
+
+import blog.do as do
+
+
+class Header(blocks.StructBlock):
+    header = blocks.CharBlock()
+
+    class Meta:
+        template = 'blog/blocks/header.html'
+        icon = 'bold'
+
+
+class Text(blocks.StructBlock):
+    text = blocks.RichTextBlock()
+
+    class Meta:
+        template = 'blog/blocks/text.html'
+        icon = 'doc-full'
+
+
+class Gallery(blocks.StructBlock):
+    pictures = blocks.ListBlock(ImageChooserBlock())
+
+    class Meta:
+        template = 'blog/blocks/gallery.html'
+        icon = 'image'
+
+
+class Picture(blocks.StructBlock):
+    picture = ImageChooserBlock()
+
+    class Meta:
+        template = 'blog/blocks/picture.html'
+        icon = 'image'
+
+
+class Aside(blocks.StructBlock):
+    aside = blocks.BlockQuoteBlock()
+
+    class Meta:
+        template = 'blog/blocks/aside.html'
+        icon = 'openquote'
+
+
+class Date(blocks.StructBlock):
+    date = blocks.DateBlock()
+
+    class Meta:
+        template = 'blog/blocks/date.html'
+        icon = 'date'
+
+
+class Caption(blocks.StructBlock):
+    caption = blocks.CharBlock()
+
+    class Meta:
+        template = 'blog/blocks/caption.html'
+        icon = 'form'
+
+
+class LocationStructValue(blocks.StructValue):
+
+    @staticmethod
+    def key():
+        return settings.GOOGLE_MAPS_V3_APIKEY
+
+    def zoom_level(self):
+        zoom_level = self.get('zoom')
+        if not zoom_level:
+            zoom_level = settings.GEO_WIDGET_ZOOM
+        return zoom_level
+
+    def center(self):
+        return self.get('location')["lat"] + ',' + self.get('location')["lng"]
+
+
+class Location(blocks.StructBlock):
+    address = blocks.CharBlock(required=False)
+    location = GeoBlock(address_field='address')
+    zoom = blocks.IntegerBlock(required=False, min_value=0, max_value=19, default=8)
+    satellite = blocks.BooleanBlock(required=False)
+
+    class Meta:
+        template = 'blog/blocks/location.html'
+        icon = 'site'
+        value_class = LocationStructValue
+
+
+class PlaceField(forms.HiddenInput):
+    address_field = None
+    id_prefix = 'id_'
+    srid = None
+
+    def __init__(self, *args, **kwargs):
+        self.address_field = kwargs.pop('address_field', self.address_field)
+        self.srid = kwargs.pop('srid', self.srid)
+        self.id_prefix = kwargs.pop('id_prefix', self.id_prefix)
+        self.zoom = kwargs.pop('zoom', settings.GEO_WIDGET_ZOOM)
+
+        super(PlaceField, self).__init__(*args, **kwargs)
+
+    class Media:
+        css = {
+            'all': ('widgets/place.css',)
+        }
+
+        js = (
+            'widgets/place.js',
+            'https://maps.google.com/maps/api/js?key={}&libraries=places&language={}'
+            .format(
+                settings.GOOGLE_MAPS_V3_APIKEY,
+                'en',
+            ),
+        )
+
+    def render(self, name, value, attrs=None):
+        out = super(PlaceField, self).render(name, value, attrs)
+
+        location = format_html(
+            '<div class="input">'
+            '<input id="_id_{}_latlng" class="geo-field-location" maxlength="250" type="text">'  # NOQA
+            '</div>',
+            name
+        )
+
+        if '-' in name:
+            namespace = name.split('-')[:-1]
+            namespace = '-'.join(namespace)
+            namespace = '{}-'.format(namespace)
+        else:
+            namespace = ''
+
+        source_selector = '#{}{}'.format(self.id_prefix, name)
+        address_selector = '#{}{}{}'.format(self.id_prefix,
+                                            namespace,
+                                            self.address_field)
+
+        data = {
+            'sourceSelector': source_selector,
+            'defaultLocation': settings.GEO_WIDGET_DEFAULT_LOCATION,
+            'addressSelector': address_selector,
+            'latLngDisplaySelector': '#_id_{}_latlng'.format(name),
+            'zoom': self.zoom,
+            'srid': self.srid,
+        }
+
+        if value and isinstance(value, six.string_types):
+            result = do.geosgeometry_str_to_struct(value)
+            if result:
+                data['defaultLocation'] = {
+                    'lat': result['y'],
+                    'lng': result['x'],
+                }
+
+        if value and Point and isinstance(value, Point):
+            data['defaultLocation'] = {
+                'lat': value.y,
+                'lng': value.x,
+            }
+
+        json_data = json.dumps(data)
+        data_id = 'geo_field_{}_data'.format(name)
+
+        return mark_safe(
+            '<script>window["{}"] = {};</script>'.format(data_id, json_data) +
+            out +
+            location +
+            '<div class="geo-field" data-data-id="{}"></div>'.format(data_id) +
+            """
+            <script>
+            (function(){
+                if (document.readyState === 'complete') {
+                    return initializeGeoFields();
+                }
+
+                $(window).on('load', function() {
+                    initializeGeoFields();
+                });
+            })();
+            </script>
+            """
+        )
+
+
+class PlaceBlock(blocks.FieldBlock):
+    def __init__(self, address_field=None, required=True, help_text=None,
+                 **kwargs):
+        self.field_options = {}
+        self.address_field = address_field
+        super(PlaceBlock, self).__init__(**kwargs)
+
+    @cached_property
+    def field(self):
+        field_kwargs = {'widget': PlaceField(
+            srid=4326,
+            id_prefix='',
+            address_field=self.address_field,
+        )}
+        field_kwargs.update(self.field_options)
+        return forms.CharField(**field_kwargs)
+
+    def clean(self, value):
+        if not value:
+            value = "SRID={};POINT({} {})".format(
+                4326,
+                settings.GEO_WIDGET_DEFAULT_LOCATION['lng'],
+                settings.GEO_WIDGET_DEFAULT_LOCATION['lat']
+            )
+        return super(PlaceBlock, self).clean(value)
+
+    def render_form(self, value, prefix='', errors=None):
+        if value and isinstance(value, dict):
+            value = "SRID={};POINT({} {})".format(value['srid'],
+                                                  value['lng'],
+                                                  value['lat'])
+        return super(PlaceBlock, self).render_form(value, prefix, errors)
+
+    def to_python(self, value):
+        if isinstance(value, dict):
+            return value
+
+        value = do.geosgeometry_str_to_struct(value)
+        value = {
+            'lat': value['y'],
+            'lng': value['x'],
+            'srid': value['srid'],
+        }
+
+        return super(PlaceBlock, self).to_python(value)
+
+
+class PlaceStructValue(blocks.StructValue):
+
+    @staticmethod
+    def key():
+        return settings.GOOGLE_MAPS_V3_APIKEY
+
+    def zoom_level(self):
+        zoom_level = self.get('zoom')
+        if not zoom_level:
+            zoom_level = settings.GEO_WIDGET_ZOOM
+        return zoom_level
+
+    def q(self):
+        return self.place
+
+
+class Place(blocks.StructBlock):
+    place = blocks.CharBlock(required=False)
+    location = PlaceBlock(address_field='place')
+    zoom = blocks.IntegerBlock(required=False, min_value=0, max_value=19, default=8)
+    satellite = blocks.BooleanBlock(required=False)
+
+    # Place IDs should be prefixed with place_id
+
+    class Meta:
+        template = 'blog/blocks/place.html'
+        icon = 'site'
+        value_class = PlaceStructValue
--- blog/models.py
+++ blog/models.py
@@ -1,288 +1,19 @@
-from django import forms
-from django.utils.functional import cached_property
 from django.db import models
-from django.conf import settings
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
-from django.utils.html import format_html
-from django.utils.safestring import mark_safe
-
-from wagtail.core.models import Page, Orderable
-from wagtail.core.fields import StreamField
-from wagtail.core import blocks
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
-from wagtail.images.edit_handlers import ImageChooserPanel
-from wagtail.images.blocks import ImageChooserBlock
-from wagtail.search import index
-from wagtail.search.models import Query
-
-from wagtailgeowidget.blocks import GeoBlock
-
-from blog.do import geosgeometry_str_to_struct
-
-import json
-import six
 
 try:
     from django.contrib.gis.geos.point import Point
 except:
     Point = None
 
-class Header(blocks.StructBlock):
-    header = blocks.CharBlock()
+from wagtail.core.models import Page, Orderable
+from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
+from wagtail.search.models import Query
 
-    class Meta:
-        template = 'blog/blocks/header.html'
-        icon = 'bold'
-
-
-class Text(blocks.StructBlock):
-    text = blocks.RichTextBlock()
-
-    class Meta:
-        template = 'blog/blocks/text.html'
-        icon = 'doc-full'
-
-
-class Gallery(blocks.StructBlock):
-    pictures = blocks.ListBlock(ImageChooserBlock())
-
-    class Meta:
-        template = 'blog/blocks/gallery.html'
-        icon = 'image'
-
-
-class Picture(blocks.StructBlock):
-    picture = ImageChooserBlock()
-
-    class Meta:
-        template = 'blog/blocks/picture.html'
-        icon = 'image'
-
-
-class Aside(blocks.StructBlock):
-    aside = blocks.BlockQuoteBlock()
-
-    class Meta:
-        template = 'blog/blocks/aside.html'
-        icon = 'openquote'
-
-
-class Date(blocks.StructBlock):
-    date = blocks.DateBlock()
-
-    class Meta:
-        template = 'blog/blocks/date.html'
-        icon = 'date'
-
-
-class Caption(blocks.StructBlock):
-    caption = blocks.CharBlock()
-
-    class Meta:
-        template = 'blog/blocks/caption.html'
-        icon = 'form'
-
-
-class LocationStructValue(blocks.StructValue):
-
-    @staticmethod
-    def key():
-        return settings.GOOGLE_MAPS_V3_APIKEY
-
-    def zoom_level(self):
-        zoom_level = self.get('zoom')
-        if not zoom_level:
-            zoom_level = settings.GEO_WIDGET_ZOOM
-        return zoom_level
-
-    def center(self):
-        return self.get('location')["lat"] + ',' + self.get('location')["lng"]
-
-
-class Location(blocks.StructBlock):
-    address = blocks.CharBlock(required=False)
-    location = GeoBlock(address_field='address')
-    zoom = blocks.IntegerBlock(required=False, min_value=0, max_value=19, default=8)
-    satellite = blocks.BooleanBlock(required=False)
-
-    class Meta:
-        template = 'blog/blocks/location.html'
-        icon = 'site'
-        value_class = LocationStructValue
-
-
-class PlaceField(forms.HiddenInput):
-    address_field = None
-    id_prefix = 'id_'
-    srid = None
-
-    def __init__(self, *args, **kwargs):
-        self.address_field = kwargs.pop('address_field', self.address_field)
-        self.srid = kwargs.pop('srid', self.srid)
-        self.id_prefix = kwargs.pop('id_prefix', self.id_prefix)
-        self.zoom = kwargs.pop('zoom', settings.GEO_WIDGET_ZOOM)
-
-        super(PlaceField, self).__init__(*args, **kwargs)
-
-    class Media:
-        css = {
-            'all': ('widgets/place.css',)
-        }
-
-        js = (
-            'widgets/place.js',
-            'https://maps.google.com/maps/api/js?key={}&libraries=places&language={}'
-            .format(
-                settings.GOOGLE_MAPS_V3_APIKEY,
-                'en',
-            ),
-        )
-
-    def render(self, name, value, attrs=None):
-        out = super(PlaceField, self).render(name, value, attrs)
-
-        location = format_html(
-            '<div class="input">'
-            '<input id="_id_{}_latlng" class="geo-field-location" maxlength="250" type="text">'  # NOQA
-            '</div>',
-            name
-        )
-
-        if '-' in name:
-            namespace = name.split('-')[:-1]
-            namespace = '-'.join(namespace)
-            namespace = '{}-'.format(namespace)
-        else:
-            namespace = ''
-
-        source_selector = '#{}{}'.format(self.id_prefix, name)
-        address_selector = '#{}{}{}'.format(self.id_prefix,
-                                            namespace,
-                                            self.address_field)
-
-        data = {
-            'sourceSelector': source_selector,
-            'defaultLocation': settings.GEO_WIDGET_DEFAULT_LOCATION,
-            'addressSelector': address_selector,
-            'latLngDisplaySelector': '#_id_{}_latlng'.format(name),
-            'zoom': self.zoom,
-            'srid': self.srid,
-        }
-
-        if value and isinstance(value, six.string_types):
-            result = geosgeometry_str_to_struct(value)
-            if result:
-                data['defaultLocation'] = {
-                    'lat': result['y'],
-                    'lng': result['x'],
-                }
-
-        if value and Point and isinstance(value, Point):
-            data['defaultLocation'] = {
-                'lat': value.y,
-                'lng': value.x,
-            }
-
-        json_data = json.dumps(data)
-        data_id = 'geo_field_{}_data'.format(name)
-
-        return mark_safe(
-            '<script>window["{}"] = {};</script>'.format(data_id, json_data) +
-            out +
-            location +
-            '<div class="geo-field" data-data-id="{}"></div>'.format(data_id) +
-            """
-            <script>
-            (function(){
-                if (document.readyState === 'complete') {
-                    return initializeGeoFields();
-                }
-
-                $(window).on('load', function() {
-                    initializeGeoFields();
-                });
-            })();
-            </script>
-            """
-        )
-
-
-class PlaceBlock(blocks.FieldBlock):
-    def __init__(self, address_field=None, required=True, help_text=None,
-                 **kwargs):
-        self.field_options = {}
-        self.address_field = address_field
-        super(PlaceBlock, self).__init__(**kwargs)
-
-    @cached_property
-    def field(self):
-        field_kwargs = {'widget': PlaceField(
-            srid=4326,
-            id_prefix='',
-            address_field=self.address_field,
-        )}
-        field_kwargs.update(self.field_options)
-        return forms.CharField(**field_kwargs)
-
-    def clean(self, value):
-        if not value:
-            value = "SRID={};POINT({} {})".format(
-                4326,
-                settings.GEO_WIDGET_DEFAULT_LOCATION['lng'],
-                settings.GEO_WIDGET_DEFAULT_LOCATION['lat']
-            )
-        return super(PlaceBlock, self).clean(value)
-
-    def render_form(self, value, prefix='', errors=None):
-        if value and isinstance(value, dict):
-            value = "SRID={};POINT({} {})".format(value['srid'],
-                                                  value['lng'],
-                                                  value['lat'])
-        return super(PlaceBlock, self).render_form(value, prefix, errors)
-
-    def to_python(self, value):
-        if isinstance(value, dict):
-            return value
-
-        value = geosgeometry_str_to_struct(value)
-        value = {
-            'lat': value['y'],
-            'lng': value['x'],
-            'srid': value['srid'],
-        }
-
-        return super(PlaceBlock, self).to_python(value)
-
-
-class PlaceStructValue(blocks.StructValue):
-
-    @staticmethod
-    def key():
-        return settings.GOOGLE_MAPS_V3_APIKEY
-
-    def zoom_level(self):
-        zoom_level = self.get('zoom')
-        if not zoom_level:
-            zoom_level = settings.GEO_WIDGET_ZOOM
-        return zoom_level
-
-    def q(self):
-        return self.place
-
-
-class Place(blocks.StructBlock):
-    place = blocks.CharBlock(required=False)
-    location = PlaceBlock(place_field='place')
-    zoom = blocks.IntegerBlock(required=False, min_value=0, max_value=19, default=8)
-    satellite = blocks.BooleanBlock(required=False)
-
-    # Place IDs should be prefixed with place_id
-
-    class Meta:
-        template = 'blog/blocks/place.html'
-        icon = 'site'
-        value_class = PlaceStructValue
+import blog.blocks as blocks
 
 
 def get_menu(page):
@@ -454,15 +185,15 @@ class BlogPostPage(Page):
 
 
     body = StreamField([
-        ('header', Header()),
-        ('date', Date()),
-        ('text', Text()),
-        ('aside', Aside()),
-        ('caption', Caption()),
-        ('gallery', Gallery()),
-        ('picture', Picture()),
-        ('location', Location()),
-        ('place', Place()),
+        ('header', blocks.Header()),
+        ('date', blocks.Date()),
+        ('text', blocks.Text()),
+        ('aside', blocks.Aside()),
+        ('caption', blocks.Caption()),
+        ('gallery', blocks.Gallery()),
+        ('picture', blocks.Picture()),
+        ('location', blocks.Location()),
+        ('place', blocks.Place()),
     ], null=True, blank=True)
 
     parent_page_types = [BlogSectionPage]
