1423888da41fd8cde2d20d3e7f4d8bf5d0f09b84
cutreth <cutreth@users.noreply.github.com>
2018-12-09 15:52:48 -0600

    Flickity statics

---
+++ static/js/lkbw.js
@@ -0,0 +1,54 @@
+// init Masonry
+
+var $grid = $('.grid').masonry({
+  itemSelector: '.grid-item', // select none at first
+  columnWidth: '.grid-sizer',
+  percentPosition: true,
+  stagger: 30,
+  // nicer reveal transition
+  visibleStyle: { transform: 'translateY(0)', opacity: 1 },
+  hiddenStyle: { transform: 'translateY(100px)', opacity: 0 },
+});
+
+// get Masonry instance
+var msnry = $grid.data('masonry');
+
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry('layout');
+});
+
+// init Infinte Scroll
+$grid.infiniteScroll({
+  path: '.pagination_next',
+  append: '.grid-item',
+  outlayer: msnry,
+  prefill: true,
+  history: false,
+});
+
+
+
+
+// external js: flickity.pkgd.js
+
+var $carousel = $('.carousel').flickity({
+  imagesLoaded: true,
+  percentPosition: false,
+});
+
+var $imgs = $carousel.find('.carousel-cell img');
+// get transform property
+var docStyle = document.documentElement.style;
+var transformProp = typeof docStyle.transform == 'string' ?
+  'transform' : 'WebkitTransform';
+// get Flickity instance
+var flkty = $carousel.data('flickity');
+
+$carousel.on( 'scroll.flickity', function() {
+  flkty.slides.forEach( function( slide, i ) {
+    var img = $imgs[i];
+    var x = ( slide.target + flkty.x ) * -1/3;
+    img.style[ transformProp ] = 'translateX(' + x  + 'px)';
+  });
+});
