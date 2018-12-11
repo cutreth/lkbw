f6370739728b57a7a1be18be432c8207767c0e5f
cutreth <cutreth@users.noreply.github.com>
2018-12-11 14:21:09 -0600

    Map pathing

--- lkbw/static/js/lkbw.js
+++ lkbw/static/js/lkbw.js
@@ -96,10 +96,28 @@ function initMap() {
     var latLng = new google.maps.LatLng(lat, lng);
     var marker = new google.maps.Marker({
       position: latLng,
-      map: map
+      map: map,
+      label: '',
+      title: String(i + 1),
     });
 
-    bounds.extend(marker.position);
+    if (i == 0) {
+      var path = []
+    }
+
+    path.push({lat: parseFloat(lat), lng: parseFloat(lng)})
+
+    var flightPath = new google.maps.Polyline({
+      path: path,
+      geodesic: true,
+      strokeColor: '#FF0000',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    });
+
+  flightPath.setMap(map);
+
+  bounds.extend(marker.position);
 
   }
 
